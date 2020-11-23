129c80686209ae0e5dc71fc11195678a55f08d34
klaus.wei <15221572751@163.com>
2020-11-23 13:56:10 +0800

    update src/main/java/com/yuw/HelloApplication.java. v1.0

--- src/main/java/com/yuw/HelloApplication.java
+++ src/main/java/com/yuw/HelloApplication.java
@@ -3,10 +3,12 @@ package com.yuw;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class HelloApplication {
 
     public static void main(String[] args) {
+        // V1.0
         SpringApplication.run(HelloApplication.class,args);
 
     }
